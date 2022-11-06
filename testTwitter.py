b5d74b157496e5f31204d092f0cb125b1035adcd
Hashim-ca <60452517+Hashim-ca@users.noreply.github.com>
2022-11-05 23:04:48 -0600

    Add files via upload

---
+++ testTwitter.py
@@ -0,0 +1,116 @@
+import requests
+import os
+import json
+
+from kafka import KafkaProducer
+
+import tweepy
+import logging
+
+ 
+producer = KafkaProducer(
+  bootstrap_servers=['XXXX'],
+  sasl_mechanism='XXXX-XXXX-XXXX',
+  security_protocol='XXXX',
+  sasl_plain_username='XXXX',
+  sasl_plain_password='XXXX',
+)
+
+
+bearer_token = " XXXX"
+
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2FilteredStreamPython"
+    return r
+
+
+def get_rules():
+    response = requests.get(
+        "https://api.twitter.com/2/tweets/search/stream/rules", auth=bearer_oauth
+    )
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot get rules (HTTP {}): {}".format(response.status_code, response.text)
+        )
+    print(json.dumps(response.json()))
+    return response.json()
+
+
+def delete_all_rules(rules):
+    if rules is None or "data" not in rules:
+        return None
+
+    ids = list(map(lambda rule: rule["id"], rules["data"]))
+    payload = {"delete": {"ids": ids}}
+    response = requests.post(
+        "https://api.twitter.com/2/tweets/search/stream/rules",
+        auth=bearer_oauth,
+        json=payload
+    )
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot delete rules (HTTP {}): {}".format(
+                response.status_code, response.text
+            )
+        )
+    print(json.dumps(response.json()))
+
+
+def set_rules(delete):
+    # You can adjust the rules if needed
+    sample_rules = [
+        {"value": "cats OR dogs", "tag": "testing1"},
+    ]
+    payload = {"add": sample_rules}
+    response = requests.post(
+        "https://api.twitter.com/2/tweets/search/stream/rules",
+        auth=bearer_oauth,
+        json=payload,
+    )
+    if response.status_code != 201:
+        raise Exception(
+            "Cannot add rules (HTTP {}): {}".format(response.status_code, response.text)
+        )
+    print(json.dumps(response.json()))
+
+
+def get_stream(set):
+    response = requests.get(
+        "https://api.twitter.com/2/tweets/search/stream", auth=bearer_oauth, stream=True,
+    )
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot get stream (HTTP {}): {}".format(
+                response.status_code, response.text
+            )
+        )
+    for response_line in response.iter_lines():
+        if response_line:
+            json_response = json.loads(response_line)
+            print(json.dumps(json_response, indent=4, sort_keys=True))
+            producer.send('Testing1', str.encode(json.dumps(json_response, indent=4, sort_keys=True)))
+            producer.flush()
+    producer.close()
+            
+
+
+def main():
+    rules = get_rules()
+    delete = delete_all_rules(rules)
+    set = set_rules(delete)
+    get_stream(set)
+    #send data to kafka KafkaProducer
+    #data = get_stream(set)
+    #convert data into json format
+
+
+
+if __name__ == "__main__":
+    main()
